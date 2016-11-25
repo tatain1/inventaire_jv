3071c13f1995822b363a22e540dccf98dc084c68
tatain1 <tatain1@hotmail.com>
2016-11-25 09:54:00 +0100

    Modification PDO

--- include/pdo.php
+++ include/pdo.php
@@ -1,7 +1,7 @@
 <?php
 
  try {
-        $pdo = new PDO('mysql:host=localhost;dbname=inventaire_kilian', "root", "", array(
+        $pdo = new PDO('mysql:host=localhost;dbname=', "", "", array(
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
